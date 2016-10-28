19fa677facb6ce63d1eab7c1e92b73f9d6919e7c
ShoutingLime <shouting.lime@gmail.com>
2016-10-28 18:25:34 +0300

    Commit 4.
    Change file lesson_3-3 (checking for equality to zero is removed)

--- lesson_3-3.js
+++ lesson_3-3.js
@@ -3,7 +3,7 @@
 for (var i = 0; i < 8; i++) {
   var res = (i % 2 === 0) ? '' : ' ';
   for (var j = 0; res.length < 8; j++) {
-      res = (j === 0 || j % 2 === 0) ? (res + '#') : (res + ' ');
+      res = (j % 2 === 0) ? (res + '#') : (res + ' ');
   }
   console.log(res);
 }
